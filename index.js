0b6bf329913f8ed253c7411123a8fff6c9b03cb6
Jessica Castaneda <pulsar.ppz70m@gmail.com>
2018-06-10 23:20:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -21,7 +21,6 @@ function sayHiToGrandma(string){
   else if (string.toUpperCase() === string){
      return 'YES INDEED!';
   }
-  else if (string === '')
-  
-  
+  else if (string === 'I love you, Grandma')
+     return 'I love you, too.';
 }
