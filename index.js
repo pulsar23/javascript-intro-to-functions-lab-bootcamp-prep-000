40d4f8bcb94b4a4a92839d7669dbc2c2c0b1d0d1
Jessica Castaneda <pulsar.ppz70m@gmail.com>
2018-06-10 23:19:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,11 +15,13 @@ function logWhisper(string){
 }
 
 function sayHiToGrandma(string){
-  if string.toLowerCase() === string){
+  if (string.toLowerCase() === string){
     return "I can't hear you!";
   }
   else if (string.toUpperCase() === string){
-     return 'YES INDEED!');
+     return 'YES INDEED!';
   }
   else if (string === '')
+  
+  
 }
